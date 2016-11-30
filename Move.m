b2c9e84d55c42a364fdc576579c2ea67779646d8
Martin Baerveldt <marbae@o7155a-03.o7155a.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2016-11-30 10:18:04 +0100

    Optimization

--- Move.m
+++ Move.m
@@ -23,65 +23,22 @@ function [bacteriaLocation, bacteriaLattice] = Move...
         
         if(nutrients(i0,j0) < nutrientThres && ...
                 nutrients(i0, j0) >= 0.1 || nutrients(i0, j0) > 1.2)
-            left            = i0-1;
-            right           = i0+1;
-            up              = j0-1;
-            down            = j0+1;
             
-            if(i0 == latticeSize)
-                right       = 1;
-            end
-            
-            if(i0 == 1)
-                left        = latticeSize;
-            end
-            
-            if(j0 == latticeSize)
-                down        = 1;
-            end
-            
-            if(j0 == 1)
-                up          = latticeSize;
-            end
+            [left, right, up, down] = Boundaries(i0,j0,latticeSize);
 
-            winningIndex    = [i0 j0];
-            winning         = 0;
+            [winning, winningIndex] = Direction(i0,j0,left,right,up,down,nutrients);
             
-            if(bacteriaLattice(left, j0) < 5 && ...
-                    winning < nutrients(left, j0))
-                winning     = nutrients(left, j0);
-                winningIndex = [left j0];
-            end
-            
-            if(bacteriaLattice(right,j0) < 5 && ...
-                    winning < nutrients(right, j0))
-                winning     = nutrients(right, j0);
-                winningIndex = [right j0];
-            end
-            
-            if(bacteriaLattice(i0,up) < 5 && winning < nutrients(i0,up))
-                winning = nutrients(i0,up);
-                winningIndex = [i0 up];
-            end
-            
-            if(bacteriaLattice(i0,down) < 5 && ...
-                    winning < nutrients(i0,down))
-                winning = nutrients(i0,down);
-                winningIndex = [i0 down];
-            end
-            
-            if(winning < inf)
+            if(winning < inf)%movement
                 bacteriaLattice(i0,j0) = bacteriaLattice(i0,j0) - 1;
                 bacteriaLocation(:,iBacteria(i)) = winningIndex;
                 temp = bacteriaLattice(winningIndex(1),winningIndex(2));
                 bacteriaLattice(winningIndex(1),winningIndex(2)) = temp+1;
-            elseif(winning >= inf)
+            elseif(winning >= inf)%Reproduction
                 bacteriaLocation = [bacteriaLocation winningIndex'];
                 temp = bacteriaLattice(winningIndex(1),winningIndex(2));
                 bacteriaLattice(winningIndex(1),winningIndex(2)) = temp+1;
             end
         end
-        
         %% Death Check
         if(nutrients(i0,j0) < 0.1)
             bacteriaLocation(:, iBacteria(i))   = [];
