f373481c4bcc89de4a977ac3454e98449b5905bf
scoobs1983 <scoobs1983@users.noreply.github.com>
2016-11-30 09:27:51 +0100

    Add files via upload

--- InitializeBacteria.m
+++ InitializeBacteria.m
@@ -3,19 +3,25 @@
 % Group 2 : PROJECT (Quorum Sensing Simulation)
 
 function [bacteriaLocation, BacteriaLattice] = ...
-    InitializeBacteria(nBacteria, BacteriaLattice)
-    % Places specified number of bacteria at random location within the
-    % bacterial lattice
+    InitializeBacteria(nBacteria, BacteriaLattice, crowdLimit)
+    % Places bacteria at random locations within the bacteria lattice,
+    % limiting the number of bacteria at a location to a set value
 
-    bacteriaLocation    = zeros(2, nBacteria);
-    width               = size(BacteriaLattice, 1);
+    bacteriaLocation    = zeros(3, nBacteria);                              % Row 1:  x (width); Row 2: y (length / height); Row 3: Nutrient Storage
+    coordinate          = size(BacteriaLattice, 1);
 
     for iBacteria = 1: nBacteria
-        i   = ceil(rand*width);
-        j   = ceil(rand*width);
+        placement = 'Unsuccessful';
         
-        if(BacteriaLattice(i, j) < 5)
-            BacteriaLattice(i, j) = BacteriaLattice(i, j) + 1;
+        while strcmp(placement,'Unsuccessful') == 1
+            i   = ceil(rand*coordinate);
+            j   = ceil(rand*coordinate);
+            
+            if(BacteriaLattice(i, j) < crowdLimit)
+                BacteriaLattice(i, j) = BacteriaLattice(i, j) + 1;
+                placement = 'Successful';
+            end
+            
         end
         bacteriaLocation(:,iBacteria) = [i j];
     
