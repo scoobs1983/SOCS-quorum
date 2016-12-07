3128215b3cf6c8e404377f43e4785913849b251d
scoobs1983 <mail.scoobs@gmail.com>
2016-12-07 11:46:44 +0100

    Updated
    Includes quorum-influenced induced respiration and feeding

--- Consumption.m
+++ Consumption.m
@@ -4,7 +4,7 @@
 
 function [nutrients, bacteriaEnergy] =  Consumption...
     (bacteriaLocation, bacteriaLattice, nutrients, bacteriaEnergy, ...
-    respRates, feedRate)
+    respRates, feedRates, signals, baseSignal, threshold)
     % Performs one round of consumption for all bacteria (according to 
     % fixed feed rate or splitting whatever is left, adjusting
     % respiration rates and energy stores as necessary
@@ -18,6 +18,13 @@ function [nutrients, bacteriaEnergy] =  Consumption...
     for i = 1 : nBacteria                               
         linIndex(i) = ...
             sub2ind([m n], bacteriaLocation(1, i), bacteriaLocation(2, i)); % Convert bacteria locations to linear index of bacteria lattice
+        if signals(linIndex(i)) >= threshold(3)                             % Check if 'quorum' achieved, adjust feed and respiration rates accordingly 
+            bacteriaEnergy(2, i)    = respRates(2);                         
+            bacteriaEnergy(3, i)    = feedRates(2);
+        else                                                                % Turn off 'quorum'
+            bacteriaEnergy(2, i)    = respRates(1);                         
+            bacteriaEnergy(3, i)    = feedRates(1);
+        end
     end
     
     for j = 1 : nLocations
@@ -25,26 +32,32 @@ function [nutrients, bacteriaEnergy] =  Consumption...
                 
         if isempty(resBacteria) == 0
             nResidents  = length(resBacteria);
-            toConsume   = nResidents*feedRate;
+            toConsume   = sum(bacteriaEnergy(3, resBacteria));
         
             if nutrients(j) >= toConsume                                    % Updates storage according to existing feed-rates
-                for k = 1 : nResidents                                     % Leaves feed-rates unchanged
-                    bacteriaEnergy(1, resBacteria(k)) = bacteriaEnergy(1, resBacteria(k)) + feedRate;
-                    bacteriaEnergy(2, resBacteria(k)) = respRates(2);                    % Ensures all bacteria not in "survival mode"
+                for k = 1 : nResidents                                      % Leaves feed-rates unchanged
+                    bacteriaEnergy(1, resBacteria(k)) = ...
+                        bacteriaEnergy(1, resBacteria(k)) + ...
+                        bacteriaEnergy(3, resBacteria(k)) - ...
+                        bacteriaEnergy(2, resBacteria(k));
                 end
-                nutrients(j) = nutrients(j) - nResidents*feedRate;
+                nutrients(j) = ...
+                    nutrients(j) - bacteriaEnergy(3, resBacteria);
                 
             elseif nutrients(j) < toConsume && nutrients(j) ~= 0            % Splits existing nutrients equally amongst resident bacteria
                 for k = 1 : nResidents
                     delta = nutrients(j)/nResidents;
-                    bacteriaEnergy(1, resBacteria(k)) = bacteriaEnergy(1, resBacteria(k)) + delta;
-                    bacteriaEnergy(2, resBacteria(k)) = respRates(1);                    % Reduces feed-rates of all resident bacteria
+                    bacteriaEnergy(1, resBacteria(k)) = ...
+                        bacteriaEnergy(1, resBacteria(k)) + delta - ...
+                        bacteriaEnergy(2, resBacteria(k));
                 end
                 nutrients(j) = 0;                                           % Leaves nutrients empty
             
             else                
-                for k = 1 : nResidents                                     % If there are no nutrients
-                    bacteriaEnergy(2, resBacteria(k)) = respRates(1);               % Reduces feed-rates of all resident bacteria
+                for k = 1 : nResidents                                      % If there are no nutrients
+                    bacteriaEnergy(1, resBacteria(k)) = ...
+                        bacteriaEnergy(1, resBacteria(k)) - ...
+                        bacteriaEnergy(2, resBacteria(k));
                 end
             end
         end
