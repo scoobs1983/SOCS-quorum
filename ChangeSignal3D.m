ea1b5b3bb6beed80625091a8f9d685275f93e213
scoobs1983 <mail.scoobs@gmail.com>
2016-12-09 17:16:39 +0100

    More 3D File Additions

---
+++ ChangeSignal3D.m
@@ -0,0 +1,28 @@
+% Simulation of Complex Systems (FFR120), 2016
+% Chalmers University of Technology
+% Group 2 : PROJECT (Quorum Sensing Simulation)
+
+function signals    = ChangeSignal3D(bacteriaLocation, signals, ...
+    neighbours, baseSignal, rho, sigThres)
+    % Description : TBC
+
+    latticeSize     = size(signals,1);
+    newSignals      = zeros(latticeSize, latticeSize, latticeSize);
+    nBacteria       = length(bacteriaLocation);
+    
+    for iBacteria = 1 : nBacteria
+        index    = bacteriaLocation(iBacteria);
+        if signals(index) > sigThres
+            newSignals(index)   = newSignals(index) + 2*baseSignal;
+            newSignals(neighbours(index, :)) = ...
+                newSignals(index) + baseSignal;
+        else
+            newSignals(index)   = newSignals(index) + baseSignal;
+            newSignals(neighbours(index, :)) = ...
+                newSignals(index) + 0.5*baseSignal;
+        end
+    end
+    signals = rho*signals + newSignals;                                     % Decay Rate of Signals
+       
+end
+     
